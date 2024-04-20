bd7ee3ac528c01cc184dec0b72b8bcfd6a53eec7
hamidreza <hamidrezakargar44@gmail.com>
2024-04-20 11:40:23 +0330

    finished booking hotel :)

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Map from "../Map/Map";
+import { useHotels } from "../context/HotelsProvider";
 
 function AppLayout() {
-    return (
-        <div className="appLayout" >
-            <div className="sidebar">
-                <Outlet />
-            </div>
-            <Map />
-        </div>
-    )
+  const { hotels } = useHotels();
+  
+  return (
+    <div className="appLayout">
+      <div className="sidebar">
+        <Outlet />
+      </div>
+      <Map markerLocations={hotels} />
+    </div>
+  );
 }
-
 export default AppLayout;
